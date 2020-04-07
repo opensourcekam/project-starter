85333ac90959183a9f4c201f292179850439edbd
Kameron Robinson <kamdroidr@me.com>
2020-04-06 23:49:51 -0400

    setup backend to my liking, add client

---
+++ server/src/utils.ts
@@ -0,0 +1,21 @@
+import { Context } from './context';
+import { userSessionIdPrefix, redisSessionPrefix } from './constants/index';
+
+export const getUserId = (ctx: Context): string => {
+	const { userId } = ctx.session;
+
+	if (userId) {
+		return userId;
+	}
+};
+
+export const removeAllUsersSessions = async (userId: string, redis: any) => {
+	const sessionIds = await redis.lrange(`${userSessionIdPrefix}${userId}`, 0, -1);
+
+	const promises = [];
+
+	for (let i = 0; i < sessionIds.length; i += 1) {
+		promises.push(redis.del(`${redisSessionPrefix}${sessionIds[i]}`));
+	}
+	await Promise.all(promises);
+};
